6dc8a4a38c05d2e55a6a14598526659c5089435f
Victor Oviedo <victor.oviedo.a87@gmail.com>
2022-06-26 22:49:26 -0300

    Proyecto de portfolio con todos los components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { HeadSuperiorComponent } from './components/head-superior/head-superior.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysSkillsComponent } from './components/hys-skills/hys-skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -14,10 +21,17 @@ import { HeadSuperiorComponent } from './components/head-superior/head-superior.
     HeaderComponent,
     AcercaDeComponent,
     HeadSuperiorComponent,
+    BannerComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HysSkillsComponent,
+    ProyectosComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
